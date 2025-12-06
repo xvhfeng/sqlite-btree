c58b2812656e0ed35ac87074c4500429efebd529
bigmouth <xvhfeng@gmail.com>
2025-12-06 14:57:06 +0800

    delete build folder

--- build/CMakeFiles/simple_sqlite.dir/core/random.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/simple_sqlite.dir/core/random.c.o: \
-  /Users/bgm/Downloads/simple-sqlite-master/core/random.c \
-  /Users/bgm/Downloads/simple-sqlite-master/core/sqliteInt.h \
-  /Users/bgm/Downloads/simple-sqlite-master/core/os.h
